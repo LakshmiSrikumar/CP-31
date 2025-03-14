12c8ca5803945a09a901bfaf05e41065fd4390ff
LakshmiSrikumar <141216093+LakshmiSrikumar@users.noreply.github.com>
2025-03-14 19:25:31 +0530

    Create coins.cpp

---
+++ 800/coins.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while (t--){
+        unsigned long long int n,k;
+        cin>>n>>k;
+        if (n==1 && k!=1) cout<<"NO"<<endl;
+        else if ( n%k==0 || n%2==0 || n%(2+k)==0 || (n-k)%2==0) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+}
