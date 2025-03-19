c60af3e8dc5023626f629306f2eed9d78a54e38e
LakshmiSrikumar <141216093+LakshmiSrikumar@users.noreply.github.com>
2025-03-20 01:09:25 +0530

    Create Good_Array.cpp

---
+++ 800/Good_Array.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x,i,j=0,ans=0;
+        cin>>n;
+        vector<int> arr;
+        for(i=0;i<n;i++){
+            cin>>x;
+            arr.push_back(x);
+        }
+        i=0;
+        while(j<n){
+            int a=arr[i]%2,c=0;
+            while(j<n && arr[j]%2==a) j++,c++;
+            ans+=c-1;
+            i=j;
+        }
+        cout<<ans<<endl;
+    }
+}
