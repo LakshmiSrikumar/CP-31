8313639f7cd4d21c35602022bf9a6167224408b7
LakshmiSrikumar <141216093+LakshmiSrikumar@users.noreply.github.com>
2025-03-23 14:57:50 +0530

    Create Walking_Master.cpp

---
+++ 800/Walking_Master.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        int s= d-b;
+        if(s<0 || a+s<c) cout<<"-1"<<endl;
+        else cout<<s+(a+s-c)<<endl;
+    }
+}
