315ca80200685f4fad8b4178b222d5a9d3f22ced
LakshmiSrikumar <141216093+LakshmiSrikumar@users.noreply.github.com>
2025-03-26 23:19:31 +0530

    Create antifibo.cpp

---
+++ constructor open cup/antifibo.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool fibonacci(int x){
+    if (x==1);
+
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        if (x==2 || x==3 || x==4 || x==5 || x==6 || x==7 || x==9){
+            cout<<"NO"<<endl;
+        }
+        else{
+            cout<<"YES"<<endl;
+        }
+    }
+}
