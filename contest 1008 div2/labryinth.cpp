fccc3f569a472300ab810240daa34af01ba29950
LakshmiSrikumar <141216093+LakshmiSrikumar@users.noreply.github.com>
2025-03-28 00:48:53 +0530

    Create labryinth.cpp

---
+++ contest 1008 div2/labryinth.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        if(k%2==1){
+            for(int i=1;i<n;i++){
+                cout<<n<<" ";
+            }
+            cout<<n-1<<endl;
+        }
+        else{
+            for(int i=1;i<n-1;i++){
+                cout<<n-1<<" ";
+            }
+            cout<<n<<" "<<n-1<<endl;
+        }
+    }
+}
